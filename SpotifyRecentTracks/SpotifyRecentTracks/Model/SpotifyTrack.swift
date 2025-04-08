8a541d6532755546657cc6530f842e41520541d5
NateWatkins <naynay7805@gmail.com>
2025-04-07 17:49:44 -0700

    4-7 Swift Integration Changes

---
+++ SpotifyRecentTracks/SpotifyRecentTracks/Model/SpotifyTrack.swift
@@ -0,0 +1,8 @@
+struct SpotifyTrack: Identifiable {
+    var id: String
+    let album: String
+    let albumArt: String
+    let artists: String
+    let name: String
+    let playedAt: String
+}
--- uploadToFirebase.py
+++ uploadToFirebase.py
@@ -7,7 +7,6 @@ import datetime
 
 
 def upload_track_to_firebase(track_info):
-        
     # Initialize Firebase app
     cred = credentials.Certificate("musicbynate-a7be3-firebase-adminsdk-fbsvc-ba6239f92a.json")
     firebase_admin.initialize_app(cred, {
